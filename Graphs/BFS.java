58e8de5ab52546aa3d2730ce34bc6bf1404bc1c7
Lucky Gupta <138562149+LuckyGupta1712@users.noreply.github.com>
2026-02-04 21:04:57 +0530

    Implementation of BFS algorithm

---
+++ Graphs/BFS.java
@@ -0,0 +1,19 @@
+void bfs(int start) {
+    boolean[] visited = new boolean[V];
+    Queue<Integer> q = new LinkedList<>();
+
+    visited[start] = true;
+    q.add(start);
+
+    while (!q.isEmpty()) {
+        int node = q.poll();
+        System.out.print(node + " ");
+
+        for (int neigh : adj.get(node)) {
+            if (!visited[neigh]) {
+                visited[neigh] = true;
+                q.add(neigh);
+            }
+        }
+    }
+}
