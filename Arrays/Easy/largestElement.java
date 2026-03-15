2c90e127b07f47e2d8819bdc8ec85b2f449fbf11
Lucky Gupta <138562149+LuckyGupta1712@users.noreply.github.com>
2026-03-15 09:57:08 +0530

    Add largest element finder in largestElement.java
    Implement a method to find the largest element in an array.

---
+++ Arrays/Easy/largestElement.java
@@ -0,0 +1,16 @@
+class Main {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5};
+        System.out.println(largest(arr));
+    }
+    public static int largest(int[] arr){
+        int max=arr[0];
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+                
+            }
+        }
+        return max;
+    }
+}
