6a7bc8e42d6f8807583843eeb1aff26a615ab3c5
amazing-lucky <amazing.lucky.g@gmail.com>
2026-06-26 16:15:09 +0530

    Code to implement postorder traversal on a binary tree

---
+++ binary_tree/postOrderTraversal.java
@@ -0,0 +1,41 @@
+package binary_tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class postOrderTraversal {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right; 
+        TreeNode(int val) { this.val = val; }
+      
+    }
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res=new ArrayList<>();
+        postorder(root,res);
+        return res;        
+    }
+    private void postorder(TreeNode root ,List<Integer> res){
+        if (root==null){
+            return;
+        }
+        
+        postorder(root.left,res);
+        postorder(root.right,res);
+        res.add(root.val);
+
+    }
+    public static void main(String[] args) {
+        postOrderTraversal solution = new postOrderTraversal();
+        
+        TreeNode root = solution.new TreeNode(1);
+        root.right = solution.new TreeNode(2);
+        root.right.left = solution.new TreeNode(3);
+        
+        List<Integer> result = solution.postorderTraversal(root);
+        System.out.println("Post-order Traversal: " + result);
+    }
+}
+
